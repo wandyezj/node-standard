816e701e19557ada3c982632d823a663b5498d9b
Joe Wandyez <jowandy@microsoft.com>
2019-08-19 11:05:37 -0700

    add capitalize string function

--- src/lib/string.ts
+++ src/lib/string.ts
@@ -26,3 +26,16 @@ export function insertTabs(string: string, count: number = 1): string {
     const tabs = "\t".repeat(count);
     return `${tabs}${string.replace(/\n/g, `\n${tabs}`)}`;
 }
+
+/**
+ * uppercases the first character in a string.
+ * in the case that the first character in the string can not be upper cased (for example a white space character or an empty string) the string is unmodified.
+ * @param string
+ * @returns the string with it's first character upper cased.
+ */
+export function capitalize(string: string): string {
+    if (string.length === 0) {
+        return string;
+    }
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
